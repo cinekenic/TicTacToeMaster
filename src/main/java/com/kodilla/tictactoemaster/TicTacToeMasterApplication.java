4d5f5bdea3f0feca99c3e9fd52014a538c281f2e
Piotr-Trzcinski <cinekenic@go2.pl>
2025-02-08 10:39:16 +0100

    Initial commit

---
+++ src/main/java/com/kodilla/tictactoemaster/TicTacToeMasterApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.tictactoemaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicTacToeMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/tictactoemaster/TicTacToeMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.tictactoemaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
