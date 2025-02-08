5654e8f901ae95b2a35ac641094e858a4c5d70dd
Piotr-Trzcinski <cinekenic@go2.pl>
2025-02-08 10:48:49 +0100

    scanner

---
+++ src/main/java/com/kodilla/tictactoemaster/tic_tac_toe/TicTacToeGame.java
@@ -0,0 +1,14 @@
+package com.kodilla.tictactoemaster.tic_tac_toe;
+
+import java.util.Scanner;
+
+public class TicTacToeGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Podaj swoje imię: ");
+        String name = scanner.nextLine();
+        System.out.println("Witaj, " + name + "!");
+
+    }
+}
