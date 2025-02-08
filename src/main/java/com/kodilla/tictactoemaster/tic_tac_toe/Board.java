91d768ca40605da8bbd28757770a12ab1f66fe2b
Piotr-Trzcinski <cinekenic@go2.pl>
2025-02-08 11:24:11 +0100

    board view

---
+++ src/main/java/com/kodilla/tictactoemaster/tic_tac_toe/Board.java
@@ -0,0 +1,29 @@
+package com.kodilla.tictactoemaster.tic_tac_toe;
+
+class Board {
+    private char[][] board;
+
+    public Board() {
+        board = new char[3][3];
+        initializeBoard();
+    }
+
+    private void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = ' ';
+            }
+        }
+    }
+
+    public void printBoard() {
+        System.out.println("-------------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println("\n-------------");
+        }
+    }
+}
--- src/main/java/com/kodilla/tictactoemaster/tic_tac_toe/TicTacToeGame.java
+++ src/main/java/com/kodilla/tictactoemaster/tic_tac_toe/TicTacToeGame.java
@@ -6,9 +6,15 @@ public class TicTacToeGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Podaj swoje imię: ");
-        String name = scanner.nextLine();
-        System.out.println("Witaj, " + name + "!");
+//        System.out.print("Podaj swoje imię: ");
+//        String name = scanner.nextLine();
+//        System.out.println("Witaj, " + name + "!");
+
+
+        Board board = new Board();
+        board.printBoard();
+
+
 
     }
 }
