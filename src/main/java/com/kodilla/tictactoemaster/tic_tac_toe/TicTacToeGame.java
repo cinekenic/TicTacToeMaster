9dae197cf78923a07424e659a7d869c368da469c
Piotr-Trzcinski <cinekenic@go2.pl>
2025-02-26 22:01:28 +0100

    javaFX

--- src/main/java/com/kodilla/tictactoemaster/tic_tac_toe/TicTacToeGame.java
+++ src/main/java/com/kodilla/tictactoemaster/tic_tac_toe/TicTacToeGame.java
@@ -1,69 +1,105 @@
 package com.kodilla.tictactoemaster.tic_tac_toe;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import java.util.Random;
 
-public class TicTacToeGame {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Wybierz tryb gry: (1) 3x3 (2) 10x10 do pięciu");
+public class TicTacToeGame extends Application {
+    private Board board;
+    private Button[][] buttons;
+    private char currentPlayer;
+    private int size;
+    private int winCondition;
 
-//        System.out.print("Podaj swoje imię: ");
-//        String name = scanner.nextLine();
-//        System.out.println("Witaj, " + name + "!");
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        VBox root = new VBox(10);
+        root.setAlignment(Pos.CENTER);
 
-        int choice = scanner.nextInt();
-        int size = (choice == 1) ? 3 : 10;
-        int winCondition = (choice == 1) ? 3 : 5;
+        Button btn3x3 = new Button("Graj na planszy 3x3");
+        btn3x3.setOnAction(e -> initializeGame(primaryStage, 3, 3));
 
-        Board board = new Board(size, winCondition);
-        char currentPlayer = 'X';
-        boolean gameOn = true;
+        Button btn10x10 = new Button("Graj na planszy 10x10");
+        btn10x10.setOnAction(e -> initializeGame(primaryStage, 10, 5));
 
+        root.getChildren().addAll(btn3x3, btn10x10);
 
-        while (gameOn) {
-        board.printBoard();
-//        System.out.println("Player " + currentPlayer + "'s turn");
-//            int row = scanner.nextInt();
-//            int col = scanner.nextInt();
-            int row = -1, col = -1;
-            boolean validInput = false;
+        Scene scene = new Scene(root, 300, 200);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Tic Tac Toe - Wybór planszy");
+        primaryStage.show();
+    }
 
-            if (currentPlayer == 'X') {
-                while (!validInput) {
-                    try {
-                        System.out.println("Gracz X, podaj wiersz i kolumnę (0-" + (size - 1) + "):");
-                        row = scanner.nextInt();
-                        col = scanner.nextInt();
-                        validInput = true;
-                    } catch (InputMismatchException e) {
-                        System.out.println("Nieprawidłowe dane, podaj dwie liczby.");
-                        scanner.nextLine();
-                    }
-                }
-            }else {
-                System.out.println("Komputer wykonuje ruch...");
-                int[] move = board.getRandomMove();
-                row = move[0];
-                col = move[1];
-            }
+    private void initializeGame(Stage primaryStage, int boardSize, int winCond){
+        this.size = boardSize;
+        this.winCondition = winCond;
+        board = new Board(size, winCondition);
+        buttons = new Button[size][size];
+        currentPlayer = 'X';
 
-            if (board.makeMove(row, col, currentPlayer)) {
-                if(board.checkWin(currentPlayer)) {
-                    board.printBoard();
-                    System.out.println("Gracz " + currentPlayer + " wygrywa!");
-                    gameOn = false;
-                } else if (board.isFull()) {
-                    board.printBoard();
-                    System.out.println("Remis");
-                    gameOn = false;
-                } else {
-                    currentPlayer = (currentPlayer == 'X' ? 'O' : 'X');
-                }
-            } else {
-                System.out.println("Nieprawidłowy ruch, spróbuj ponownie.");
+        GridPane gridPane = new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                Button button = new Button(" ");
+                button.setMinSize(50, 50);
+                final int row = i;
+                final int col = j;
+                button.setOnAction(e -> makeMove(row, col, button));
+                buttons[i][j] = button;
+                gridPane.add(button, j, i);
             }
         }
-        scanner.close();
+
+        Scene scene = new Scene(gridPane, size * 50, size * 50);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Tic Tac Toe - Plansza " + size + "x" + size);
+    }
+
+    private void makeMove(int row, int col, Button button){
+        if (board.makeMove(row, col, currentPlayer)) {
+            button.setText(String.valueOf(currentPlayer));
+            if (board.checkWin(currentPlayer)) {
+                System.out.println("Gracz " + currentPlayer + " wygrywa!");
+                disableBoard();
+                return;
+            } else if (board.isFull()) {
+                System.out.println("Remis!");
+                disableBoard();
+                return;
+            }
+            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            if(currentPlayer == 'O'){
+                makeComputerMove();
+            }
+        }
+    }
+
+    private void makeComputerMove(){
+        Random random = new Random();
+        int row, col;
+        do {
+            row = random.nextInt(size);
+            col = random.nextInt(size);
+        } while (buttons[row][col].getText().equals("X") || buttons[row][col].getText().equals("O"));
+        makeMove(row, col, buttons[row][col]);
+    }
+
+    private void disableBoard(){
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                buttons[i][j].setDisable(true);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
